7b4ace6752b64cb05556d7d053a0a66ebc080bb9
Cordykc55 <cordelia.agwu@gmail.com>
2026-07-07 20:56:40 +0100

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,18 @@
+console.log("Hello, this is a JavaScript string assignment");
+
+let message = prompt(`Enter your message`);
+console.log(message);
+let str = "Both compilers and interpreters translate human-readable";
+console.log(str.toLowerCase());
+console.log(str.length);
+console.log(str.slice(6));
+console.log(str.slice(50));
+
+console.log("Enter Your Full Name");
+let firstName = "aMa";
+let middleName = "cHinedU";
+let lastName = "OgboNnaYa";
+let fullName = firstName + " " + middleName +" "+ lastName;
+console.log(fullName);
+let str2 = `aMa cHinedU OgboNnaYa`;
+console.log(str2.toUpperCase());
